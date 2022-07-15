c702a50c52c33e7e915cf7c2a825d595d86ea203
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2022-07-15 14:09:24 +0100

    Fix test

--- OpenActive.DatasetSite.NET.Tests/SimpleImplementation.cs
+++ OpenActive.DatasetSite.NET.Tests/SimpleImplementation.cs
@@ -15,50 +15,52 @@ namespace OpenActive.DatasetSite.NET.Tests
             this.output = output;
         }
 
-        private string html = "";
+        // Customer-specific settings for dataset JSON (these should come from a database)
+        private DatasetSiteGeneratorSettings settings = new DatasetSiteGeneratorSettings
+        {
+            OpenDataFeedBaseUrl = "https://customer.example.com/feed".ParseUrlOrNull(),
+            OpenBookingAPIBaseUrl = "https://customer.example.com/api/openbooking".ParseUrlOrNull(),
+            OpenBookingAPIAuthenticationAuthorityUrl = "https://auth.acmebooker.example.com".ParseUrlOrNull(),
+            DatasetSiteUrl = "https://halo-odi.legendonlineservices.co.uk/openactive/".ParseUrlOrNull(),
+            DatasetDiscussionUrl = "https://github.com/gll-better/opendata".ParseUrlOrNull(),
+            DatasetDocumentationUrl = "https://docs.acmebooker.example.com/".ParseUrlOrNull(),
+            DatasetLanguages = new List<string> { "en-GB" },
+            OrganisationName = "Better",
+            OrganisationUrl = "https://www.better.org.uk/".ParseUrlOrNull(),
+            OrganisationLegalEntity = "GLL",
+            OrganisationPlainTextDescription = "Established in 1993, GLL is the largest UK-based charitable social enterprise delivering leisure, health and community services. Under the consumer facing brand Better, we operate 258 public Sports and Leisure facilities, 88 libraries, 10 children�s centres and 5 adventure playgrounds in partnership with 50 local councils, public agencies and sporting organisations. Better leisure facilities enjoy 46 million visitors a year and have more than 650,000 members.",
+            OrganisationLogoUrl = "http://data.better.org.uk/images/logo.png".ParseUrlOrNull(),
+            OrganisationEmail = "info@better.org.uk",
+            PlatformName = "AcmeBooker",
+            PlatformUrl = "https://acmebooker.example.com/".ParseUrlOrNull(),
+            PlatformVersion = "2.0",
+            BackgroundImageUrl = "https://data.better.org.uk/images/bg.jpg".ParseUrlOrNull(),
+            DateFirstPublished = new DateTimeOffset(new DateTime(2019, 01, 14))
+        };
+
+        private List<OpportunityType> supportedFeeds = new List<OpportunityType> {
+            OpportunityType.SessionSeries,
+            OpportunityType.ScheduledSession,
+            OpportunityType.FacilityUse,
+            OpportunityType.FacilityUseSlot,
+            OpportunityType.CourseInstance
+        };
 
         [Fact]
-        public void SettingsContainedInRenderedPage()
+        public void SettingsContainedInSingleFileTemplateRenderedPage()
         {
-            // Customer-specific settings for dataset JSON (these should come from a database)
-            var settings = new DatasetSiteGeneratorSettings
-            {
-                OpenDataFeedBaseUrl = "https://customer.example.com/feed".ParseUrlOrNull(),
-                OpenBookingAPIBaseUrl = "https://customer.example.com/api/openbooking".ParseUrlOrNull(),
-                OpenBookingAPIAuthenticationAuthorityUrl = "https://auth.acmebooker.example.com".ParseUrlOrNull(),
-                DatasetSiteUrl = "https://halo-odi.legendonlineservices.co.uk/openactive/".ParseUrlOrNull(),
-                DatasetDiscussionUrl = "https://github.com/gll-better/opendata".ParseUrlOrNull(),
-                DatasetDocumentationUrl = "https://docs.acmebooker.example.com/".ParseUrlOrNull(),
-                DatasetLanguages = new List<string> { "en-GB" },
-                OrganisationName = "Better",
-                OrganisationUrl = "https://www.better.org.uk/".ParseUrlOrNull(),
-                OrganisationLegalEntity = "GLL",
-                OrganisationPlainTextDescription = "Established in 1993, GLL is the largest UK-based charitable social enterprise delivering leisure, health and community services. Under the consumer facing brand Better, we operate 258 public Sports and Leisure facilities, 88 libraries, 10 children�s centres and 5 adventure playgrounds in partnership with 50 local councils, public agencies and sporting organisations. Better leisure facilities enjoy 46 million visitors a year and have more than 650,000 members.",
-                OrganisationLogoUrl = "http://data.better.org.uk/images/logo.png".ParseUrlOrNull(),
-                OrganisationEmail = "info@better.org.uk",
-                PlatformName = "AcmeBooker",
-                PlatformUrl = "https://acmebooker.example.com/".ParseUrlOrNull(),
-                PlatformVersion = "2.0",
-                BackgroundImageUrl = "https://data.better.org.uk/images/bg.jpg".ParseUrlOrNull(),
-                DateFirstPublished = new DateTimeOffset(new DateTime(2019, 01, 14))
-            };
-
-            var supportedFeeds = new List<OpportunityType> {
-                OpportunityType.SessionSeries,
-                OpportunityType.ScheduledSession,
-                OpportunityType.FacilityUse,
-                OpportunityType.FacilityUseSlot,
-                OpportunityType.CourseInstance
-            };
-
             var html = DatasetSiteGenerator.RenderSimpleDatasetSite(settings, supportedFeeds);
             output.WriteLine(html);
             Assert.Contains(settings.OrganisationPlainTextDescription, html);
+        }
 
-            var html2 = DatasetSiteGenerator.RenderSimpleDatasetSite(settings, supportedFeeds, "./example-assets-directory/");
-            output.WriteLine(html2);
-            // Slash is removed and hierarchical path normalised
-            Assert.Contains("\"./example-assets-directory/datasetsite.styles", html2);
+        [Fact]
+        public void SettingsContainedInCspCompatibleRenderedPage()
+        {
+            var html = DatasetSiteGenerator.RenderSimpleDatasetSite(settings, supportedFeeds, "./example-assets-directory/");
+            output.WriteLine(html);
+            // Slash should be removed, which this asserts
+            Assert.Contains("\"./example-assets-directory/datasetsite.styles", html);
         }
     }
 }
